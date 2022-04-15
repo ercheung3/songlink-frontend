074f3bc5b9dee524e45a836e1945b0c8153f284b
ercheung3 <ercheung3@gmail.com>
2022-04-15 17:11:51 -0400

    Added lyrics get route

---
+++ src/LyricsContainer/SingleLyricsComponent/singleLyricsComponent.jsx
@@ -0,0 +1,9 @@
+const SingleLyricsComponent = (props) => {
+  return (
+    <div>
+      <h1>{props.lyrics.title}</h1>
+    </div>
+  );
+};
+
+export default SingleLyricsComponent;
--- src/LyricsContainer/lyricsContainer.jsx
+++ src/LyricsContainer/lyricsContainer.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-
+import SingleLyricsComponent from "./SingleLyricsComponent/singleLyricsComponent";
 const LyricsContainer = () => {
   const [lyrics, setLyrics] = useState([]);
+
+  const websiteURL = "http://localhost:8000/api/lyrics";
   const getLyrics = async () => {
-    const getLyricsApiResponse = await fetch(
-      "http://lolcahost:8000/api/lyrics"
-    );
+    const getLyricsApiResponse = await fetch(websiteURL);
     const parsedLyrics = await getLyricsApiResponse.json();
     setLyrics(parsedLyrics);
   };
@@ -14,7 +14,17 @@ const LyricsContainer = () => {
     <div>
       <h2>Lyrics Container</h2>
       <button onClick={getLyrics}>Get Lyrics</button>
-      <p>The lyrics are: {JSON.stringify(lyrics)}</p>
+      {lyrics.map((lyrics) => {
+        return (
+          console.log(lyrics.title),
+          (
+            <SingleLyricsComponent
+              key={`lyrics-${lyrics.id}`}
+              lyrics={lyrics}
+            ></SingleLyricsComponent>
+          )
+        );
+      })}
     </div>
   );
 };
